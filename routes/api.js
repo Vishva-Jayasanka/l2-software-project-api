196ff390c7da014c3bcd35fe81e18c3cba410941
Vishwa Jayasanka <vishvajayasanka@gmail.com>
2021-01-29 16:47:17 +0530

    10/27/2020 6:19 PM

--- routes/api.js
+++ routes/api.js
@@ -195,6 +195,7 @@ router.post('/get-detailed-attendance', verifyToken, async (request, response) =
                 if (error) {
                     response.status(500).send(Errors.serverError);
                 } else {
+                    console.log(result.recordset);
                     response.status(200).send(result.recordset);
                 }
             });
